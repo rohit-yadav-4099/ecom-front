8d6cb5dd4503f710a5f69c4bbda5d21826d48c8c
Rohit Yadav <131008606+rohit-yadav-4099@users.noreply.github.com>
2023-12-19 20:06:55 +0530

    Delete src/Component directory

--- src/Component/Private.js
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateCompo =()=>{
-  
-    const auth=localStorage.getItem("token")
-    return auth ? <Outlet/> : <Navigate to="/register"/>
-}
-export default PrivateCompo;
